394fb4235607ed4d4db4941dc8d162854f2f708c
matheus-vieiras <matheusvieira.contato@hotmail.com>
2024-11-03 13:47:53 -0300

    Inclusao de outras operacoes e refatoracao do codigo

--- 05_FirstStepsInJavaWithSpringBoot/rest-with-spring-boot-and-java/src/main/java/br/com/projeto/aprendizado/MathController.java
+++ 05_FirstStepsInJavaWithSpringBoot/rest-with-spring-boot-and-java/src/main/java/br/com/projeto/aprendizado/MathController.java
@@ -7,34 +7,68 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import br.com.projeto.aprendizado.converters.NumberConverter;
 import br.com.projeto.aprendizado.exceptions.UnsupportedMathOperationException;
+import br.com.projeto.aprendizado.math.SimpleMath;
 
 @RestController
 public class MathController {
 
 	private final AtomicLong counter = new AtomicLong();
 
-	@RequestMapping(value = "/sum/{numberOne}/{numberTwo}", method = RequestMethod.GET)
-	public Double sum(@PathVariable(value = "numberOne") String numberOne,
-			@PathVariable(value = "numberTwo") String numberTwo) throws Exception {
-		
-		if (!isNumeric(numberOne) || !isNumeric(numberTwo)) {
+	private SimpleMath math = new SimpleMath();
+
+	@RequestMapping(value = "/{operation}/{numberOne}/{numberTwo}", method = RequestMethod.GET)
+	public Double calculate(@PathVariable(value = "operation") String operation,
+							@PathVariable(value = "numberOne") String numberOne,
+							@PathVariable(value = "numberTwo") String numberTwo) throws Exception {
+
+		switch (operation.toLowerCase()) {
+		case "sum": {
+			if (!NumberConverter.isNumeric(numberOne) || !NumberConverter.isNumeric(numberTwo)) {
+				throw new UnsupportedMathOperationException("Please set a numeric value!");
+			}
+			return math.sum(NumberConverter.convertToDouble(numberOne), NumberConverter.convertToDouble(numberTwo));
+		}
+		case "subtract": {
+			if (!NumberConverter.isNumeric(numberOne) || !NumberConverter.isNumeric(numberTwo)) {
+				throw new UnsupportedMathOperationException("Please set a numeric value!");
+			}
+			return math.subtraction(NumberConverter.convertToDouble(numberOne),
+					NumberConverter.convertToDouble(numberTwo));
+		}
+		case "multiply": {
+			if (!NumberConverter.isNumeric(numberOne) || !NumberConverter.isNumeric(numberTwo)) {
+				throw new UnsupportedMathOperationException("Please set a numeric value!");
+			}
+			return math.multiplication(NumberConverter.convertToDouble(numberOne),
+					NumberConverter.convertToDouble(numberTwo));
+		}
+		case "divide": {
+			if (!NumberConverter.isNumeric(numberOne) || !NumberConverter.isNumeric(numberTwo)) {
+				throw new UnsupportedMathOperationException("Please set a numeric value!");
+			}
+			if (Double.valueOf(numberOne) == 0 || Double.valueOf(numberTwo) == 0) {
+				throw new UnsupportedMathOperationException("Division by zero is not allowed!");
+			}
+			return math.division(NumberConverter.convertToDouble(numberOne),
+					NumberConverter.convertToDouble(numberTwo));
+		}
+		default:
+			throw new IllegalArgumentException("Unexpected value: " + operation.toLowerCase());
+		}
+	}
+
+	// Rota separada para a raiz quadrada
+	@RequestMapping(value = "/sqrt/{number}", method = RequestMethod.GET)
+	public Double squareRoot(@PathVariable(value = "number") String number) throws Exception {
+		if (!NumberConverter.isNumeric(number)) {
 			throw new UnsupportedMathOperationException("Please set a numeric value!");
 		}
-		return convertToDouble(numberOne) + convertToDouble(numberTwo);
-	}
-
-	private Double convertToDouble(String strNumber) {
-		if (strNumber == null) return 0D;
-		String number = strNumber.replaceAll(",", ".");
-		if (isNumeric(number)) return Double.parseDouble(number);
-		return 0D;
-	}
-
-	private boolean isNumeric(String strNumber) {
-		if (strNumber == null) return false;
-		String number = strNumber.replaceAll(",", ".");
-		return number.matches("[-+]?[0-9]*\\.?[0-9]+");
+		if (Double.valueOf(number) < 0) {
+			throw new UnsupportedMathOperationException("Cannot calculate the square root of a negative number!");
+		}
+		return math.squareRoot(NumberConverter.convertToDouble(number));
 	}
 
 }
