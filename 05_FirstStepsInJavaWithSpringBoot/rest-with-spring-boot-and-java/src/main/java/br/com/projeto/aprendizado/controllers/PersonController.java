4e4047fa83dbbf86e9abd300532d2d5ae9c4ccb0
matheus-vieiras <matheusvieira.contato@hotmail.com>
2024-11-09 11:15:42 -0300

    Persistindo dados no banco e ajustando as classes

--- 05_FirstStepsInJavaWithSpringBoot/rest-with-spring-boot-and-java/src/main/java/br/com/projeto/aprendizado/controllers/PersonController.java
+++ 05_FirstStepsInJavaWithSpringBoot/rest-with-spring-boot-and-java/src/main/java/br/com/projeto/aprendizado/controllers/PersonController.java
@@ -22,7 +22,7 @@ public class PersonController {
 //	private PersonServices service = new PersonService(); a anotação ja faz a instanciação em tempo de execução
 
 	@RequestMapping(value = "/{id}", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
-	public Person findById(@PathVariable(value = "id") String id) throws Exception {
+	public Person findById(@PathVariable(value = "id") Long id) throws Exception {
 		return service.findById(id);
 	}
 
@@ -42,7 +42,7 @@ public class PersonController {
 	}
 
 	@RequestMapping(value = "/{id}", method = RequestMethod.DELETE)
-	public void delete(@PathVariable(value = "id") String id) {
+	public void delete(@PathVariable(value = "id") Long id) {
 		service.delete(id);
 	}
 
--- 05_FirstStepsInJavaWithSpringBoot/rest-with-spring-boot-and-java/src/main/java/br/com/projeto/aprendizado/services/PersonServices.java
+++ 05_FirstStepsInJavaWithSpringBoot/rest-with-spring-boot-and-java/src/main/java/br/com/projeto/aprendizado/services/PersonServices.java
@@ -1,75 +1,74 @@
 package br.com.projeto.aprendizado.services;
 
-import java.util.ArrayList;
 import java.util.List;
-import java.util.concurrent.atomic.AtomicLong;
 import java.util.logging.Logger;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import br.com.projeto.aprendizado.exceptions.ResourceNotFoundExceptionException;
 import br.com.projeto.aprendizado.model.Person;
+import br.com.projeto.aprendizado.repositories.PersonRepository;
 
 @Service
 public class PersonServices {
 
-	private final AtomicLong counter = new AtomicLong();
 	private Logger logger = Logger.getLogger(PersonServices.class.getName());
+
+	@Autowired
+	PersonRepository repository;
 
 	public List<Person> findAll() {
 
 		logger.info("Finding all people!");
-		List<Person> persons = new ArrayList<>();
 
-		for (int i = 0; i < 8; i++) {
-			Person person = mockPerson(i);
-			persons.add(person);
-		}
-
-		return persons;
+		return repository.findAll();
 	}
 
-	public Person findById(String id) {
+	public Person findById(Long id) {
 
 		logger.info("Finding one person!");
 
 		Person person = new Person();
-		person.setId(counter.incrementAndGet());
 		person.setFirstName("Matheus");
 		person.setLastName("Vieira");
 		person.setAddress("Praia Grande / São Paulo - Brasil");
 		person.setGender("Male");
 
-		return person;
+		return repository.findById(id)
+				.orElseThrow(() -> new ResourceNotFoundExceptionException("No records found for this ID"));
 	}
 
 	public Person create(Person person) {
 
 		logger.info("Creating one person!");
 
-		return person;
+		return repository.save(person);
 	}
 
 	public Person update(Person person) {
 
 		logger.info("Update one person!");
 
-		return person;
+		var entity = repository.findById(person.getId())
+				.orElseThrow(() -> new ResourceNotFoundExceptionException("No records found for this ID"));
+
+		entity.setFirstName(person.getFirstName());
+		entity.setLastName(person.getLastName());
+		entity.setAddress(person.getAddress());
+		entity.setGender(person.getGender());
+
+		return repository.save(person);
 	}
-	
-	public void delete(String id) {
+
+	public void delete(Long id) {
 		logger.info("deleting one person!");
-	}
 
-	private Person mockPerson(int i) {
+		var entity = repository.findById(id)
+				.orElseThrow(() -> new ResourceNotFoundExceptionException("No records found for this ID"));
 
-		Person person = new Person();
-		person.setId(counter.incrementAndGet());
-		person.setFirstName("Person name " + i);
-		person.setLastName("Last name " + i);
-		person.setAddress("Some address in Brasil " + i);
-		person.setGender("Male");
+		repository.delete(entity);
 
-		return person;
 	}
 
 }
