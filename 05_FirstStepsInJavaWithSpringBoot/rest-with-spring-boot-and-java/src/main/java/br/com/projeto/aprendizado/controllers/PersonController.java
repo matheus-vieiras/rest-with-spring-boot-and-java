ab63b6aba29c954f7c2e34bf348dd2a6a866c04e
matheus-vieiras <matheusvieira.contato@hotmail.com>
2024-12-14 13:53:32 -0300

    Habilitando CORS no spring

--- 05_FirstStepsInJavaWithSpringBoot/rest-with-spring-boot-and-java/src/main/java/br/com/projeto/aprendizado/controllers/PersonController.java
+++ 05_FirstStepsInJavaWithSpringBoot/rest-with-spring-boot-and-java/src/main/java/br/com/projeto/aprendizado/controllers/PersonController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -24,6 +25,7 @@ import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.tags.Tag;
 
+//@CrossOrigin
 @RestController
 @RequestMapping("/api/person/v1")
 @Tag(name = "People", description = "Endpoints for Managing People")
@@ -53,7 +55,7 @@ public class PersonController {
 	}
 	
 	
-
+	@CrossOrigin(origins = "http://localhost:8080")
 	@GetMapping(value = "/{id}", produces = { MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML, MediaType.APPLICATION_YML })
 	@Operation(summary = "Finds a Person", description = "Finds a Person", tags = { "People" }, 
 	responses = {
@@ -71,7 +73,7 @@ public class PersonController {
 	}
 	
 	
-
+	@CrossOrigin(origins = {"http://localhost:8080", "https://github.com/matheus-vieiras?tab=repositories"})
 	@Operation(summary = "Adds a new Person", description = "Adds a new Person by passing in a JSON, XML, or YML", tags = { "People" }, 
 	responses = {
 			@ApiResponse(description = "Success", responseCode = "200",
