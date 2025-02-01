c4c660359b9bdd7507b264f8eeb7927496d10815
matheus-vieiras <matheusvieira.contato@hotmail.com>
2025-02-01 12:34:13 -0300

    Preparando o serviço para prover downloads

--- projectJavaWithSpringBoot/rest-with-spring-boot-and-java/src/main/java/br/com/projeto/aprendizado/services/FileStorageService.java
+++ projectJavaWithSpringBoot/rest-with-spring-boot-and-java/src/main/java/br/com/projeto/aprendizado/services/FileStorageService.java
@@ -6,12 +6,15 @@ import java.nio.file.Paths;
 import java.nio.file.StandardCopyOption;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.UrlResource;
 import org.springframework.stereotype.Service;
 import org.springframework.util.StringUtils;
 import org.springframework.web.multipart.MultipartFile;
 
 import br.com.projeto.aprendizado.config.FileStorageConfig;
 import br.com.projeto.aprendizado.exceptions.FileStorageException;
+import br.com.projeto.aprendizado.exceptions.MyFileNotFoundException;
 
 @Service
 public class FileStorageService {
@@ -41,6 +44,17 @@ public class FileStorageService {
 			return filename;
 		} catch (Exception e) {
 			throw new FileStorageException("Could not store file " + filename + ". Please try again!", e);
+		}
+	}
+	
+	public Resource loadFileAsResource(String filename) {
+		try {
+			Path filePath = this.fileStorageLocation.resolve(filename).normalize();
+			Resource resource = new UrlResource(filePath.toUri());
+			if (resource.exists()) return resource;
+			else throw new MyFileNotFoundException("File not found");
+		} catch (Exception e) {
+			throw new MyFileNotFoundException("File not found " + filename, e);
 		}
 	}
 
