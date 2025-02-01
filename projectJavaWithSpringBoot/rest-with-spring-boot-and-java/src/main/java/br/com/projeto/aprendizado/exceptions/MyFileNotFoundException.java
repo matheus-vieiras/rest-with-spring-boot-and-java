701fc07370c18ba305d656e157a8e9bee085da72
matheus-vieiras <matheusvieira.contato@hotmail.com>
2025-02-01 11:30:37 -0300

    Criando serviço responsável por armazenar o arquivo em disco

--- projectJavaWithSpringBoot/rest-with-spring-boot-and-java/src/main/java/br/com/projeto/aprendizado/exceptions/MyFileNotFoundException.java
+++ projectJavaWithSpringBoot/rest-with-spring-boot-and-java/src/main/java/br/com/projeto/aprendizado/exceptions/MyFileNotFoundException.java
@@ -3,7 +3,7 @@ package br.com.projeto.aprendizado.exceptions;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
-@ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+@ResponseStatus(HttpStatus.NOT_FOUND)
 public class MyFileNotFoundException extends RuntimeException {
 
 	private static final long serialVersionUID = 1L;
